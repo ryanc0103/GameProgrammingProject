36c384dca2be4165e3a858def259080c6cfc21ed
cramseyy <cr22427@georgiasouthern.edu>
2025-11-12 20:24:58 -0500

    put files in correct locations

---
+++ FinalProject/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Player
+{
+    public float moveSpeed = 8f;
+
+    public GameObject laserPrefab;
+    public Transform firePoint;
+    public float fireRate = 0.25f;
+
+    private float nextFireTime = 0f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveInput = new Vector2(moveX, moveY).normalized;
+
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextFireTime)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        nextFireTime = Time.time + fireRate;
+        Instantiate(laserPrefab, firePoint.position, firePoint.rotation);
+    }
+}
