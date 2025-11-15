b96390d47a5ce855613948c401e41c4eb9b82534
cramseyy <cr22427@georgiasouthern.edu>
2025-11-15 00:21:09 -0500

    Deleted PlayerLaser.cs

--- FinalProject/Assets/Scripts/Laser.cs
+++ FinalProject/Assets/Scripts/Laser.cs
@@ -14,13 +14,13 @@ public class Laser : MonoBehaviour
     {
         Vector2 position = transform.position;
 
-        position = new Vector2 (position.x, position.y + speed * Time.deltaTime);
+        position = new Vector2(position.x, position.y + speed * Time.deltaTime);
 
         transform.position = position;
 
         Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
 
-        if(transform.position.y > max.y)
+        if (transform.position.y > max.y)
         {
             Destroy(gameObject);
         }
